a70200db03cae6c60418746f7fca28bea6576f5a
chandrika133 <160617757+chandrika133@users.noreply.github.com>
2024-02-20 20:48:20 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,30 @@
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('.buttons');
+let specialchars = ['%','*','/','-','+','='];
+let output ="";
+
+const calculate = (btnValue) => {
+
+    if(btnValue ==='=' && output !==""){
+        output = eval(output.replace("%","/100"));
+
+    }
+    else if(btnValue === "AC"){
+        output = "";
+    }
+    else if(btnValue === "DEL"){
+        output = output.toString().slice(0,-1);
+    }
+    else{
+        if(output ==="" && specialchars.includes(btnValue)) return;
+        output +=btnValue;
+    }
+    display.value = output;
+}
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click',(e) => {
+        calculate(e.target.dataset.value);
+    })
+})
